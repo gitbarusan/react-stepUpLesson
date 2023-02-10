285511895ac8090cd9b89ba36627bb90cb771ae0
gitbarusan <101911557+gitbarusan@users.noreply.github.com>
2023-02-11 07:11:23 +0900

    Initial commit

---
+++ src/components/ChildArea.jsx
@@ -0,0 +1,27 @@
+import { memo } from "react";
+
+const style = {
+  width: "100%",
+  height: "200px",
+  backgroundColor: "khaki"
+};
+
+// memoで囲むことでpropsが変更された場合のみレンダリングされる
+export const ChileArea = memo((props) => {
+  const { open } = props;
+  console.log("ChileAreaが再レンダリングされた!!");
+
+  const data = [...Array(200).keys()];
+  data.forEach((value) => {
+    console.log(value);
+  });
+  return (
+    <>
+      {open ? (
+        <div style={style}>
+          <p>子コンポーネント</p>
+        </div>
+      ) : null}
+    </>
+  );
+});
